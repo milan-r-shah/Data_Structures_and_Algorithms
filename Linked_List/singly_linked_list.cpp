dbcedbb7fa522505acee48aaf150a316fa7b7019
Milan Shah <mrshah@wpi.edu>
2020-06-08 12:33:08 -0400

    Updated Node class & added addNodeAtBeginning method in LinkedList class
    01. Node class: added 2 overloaded constructor in 
    02. LinkedList class: 
       updated name of the first node 'n1' to 'head'
       made  printLinkedList method 'const'
       added addNodeAtBeginning method

--- Linked_List/singly_linked_list.cpp
+++ Linked_List/singly_linked_list.cpp
@@ -5,16 +5,19 @@ class Node {
 public:
     int data;
     Node* next;
+    Node() : data{0} {};
+    Node(int data) : data{data} {}
 };
 
 // LinkedList class
 class LinkedList {
 public:
     LinkedList();
-    void printLinkedList();
+    void printLinkedList() const;
+    void addNodeAtBeginning(int data);
 
 private:
-    Node* n1;
+    Node* head;
     Node* n2;
     Node* n3;
     Node* n4;
@@ -23,35 +26,45 @@ private:
 // LinkedList constructor
 LinkedList::LinkedList() {
     // Creating 4 nodes
-    n1 = new Node();
+    head = new Node();
     n2 = new Node();
     n3 = new Node();
     n4 = new Node();
-    
-    n1->data = 10;
-    n1->next = n2;
+
+    head->data = 10;
+    head->next = n2;
 
     n2->data = 20;
     n2->next = n3;
 
     n3->data = 30;
     n3->next = n4;
-    
+
     n4->data = 40;
     n4->next = nullptr;
 }
 
-void LinkedList::printLinkedList() {
-    Node* tempNode = n1;
-    while(tempNode) {
+// LinkedList class method for printing LinkedList
+void LinkedList::printLinkedList() const {
+    Node* tempNode = head;
+    while (tempNode) {
         std::cout << tempNode->data << " --> ";
-        tempNode = tempNode->next; 
+        tempNode = tempNode->next;
     }
     std::cout << "nullptr\n";
 }
 
+// LinkedList class method for adding a Node at the beginning
+void LinkedList::addNodeAtBeginning(int data) {
+    Node* newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+}
+
 int main() {
     LinkedList llobj;
+    llobj.printLinkedList();
+    llobj.addNodeAtBeginning(13);
     llobj.printLinkedList();
 
     return 0;
